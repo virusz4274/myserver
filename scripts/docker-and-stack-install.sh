e385e1e8414af5a3aba616e5361fa3e5b6468c5d
virusz4274 <43103921+virusz4274@users.noreply.github.com>
2021-08-28 00:59:44 +0530

    npm stack auto  deploy
    modified script to deploy npm stack in docker.

--- scripts/docker-and-stack-install.sh
+++ scripts/docker-and-stack-install.sh
@@ -6,3 +6,7 @@ sudo usermod -aG docker $USER
 sudo apt install -y apache2-utils
 sudo apt-get -y install python3 python3-pip
 sudo pip3 install docker-compose
+mkdir npm-stack
+cd npm-stack
+wget https://raw.githubusercontent.com/virusz4274/myserver/main/docker-stacks/npm-stack/docker-compose.yml
+sudo docker-compose up -d
