5a0d15a868e8ba993fbab24018de332062b60f93
virusz4274 <43103921+virusz4274@users.noreply.github.com>
2021-08-28 16:22:59 +0530

    Update docker-and-stack-install.sh

--- scripts/docker-and-stack-install.sh
+++ scripts/docker-and-stack-install.sh
@@ -6,6 +6,8 @@ sudo usermod -aG docker $USER
 sudo apt install -y apache2-utils
 sudo apt-get -y install python3 python3-pip
 sudo pip3 install docker-compose
+sudo docker volume create portainer_data
+sudo docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 mkdir npm-stack
 cd npm-stack
 wget https://raw.githubusercontent.com/virusz4274/myserver/main/docker-stacks/npm-stack/docker-compose.yml
